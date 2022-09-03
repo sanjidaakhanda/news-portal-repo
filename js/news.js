2b01c7f72e868b5c3c12f68f11bb9029a362fba0
Your Name <sanjida.ier@gmail.com>
2022-09-02 22:01:15 -0400

    final nav  commit

--- js/news.js
+++ js/news.js
@@ -1,1 +1,87 @@
-console.log('news')
+// console.log('news')
+
+const loadNews = async()=>{
+const url = `https://openapi.programming-hero.com/api/news/categories`
+
+
+const res = await fetch(url) 
+const data = await res.json()
+  return data.data.news_category
+}
+
+ const setAllNews = async()=>{
+    const data = await loadNews()
+const menu = document.getElementById("news-menu")
+//   console.log(data)
+const uniqueArray = []
+for(const news of data ){
+    //  console.log(uniqueArray.indexOf(news.category_name))
+if (uniqueArray.indexOf( news.category_name) === -1){
+uniqueArray.push(news.category_name)
+const li = document.createElement('li')
+li.innerHTML =`
+<a class="ms-5" onclick = "allcategories('${news.category_id}')">${news.category_name}</a>
+
+`
+menu.appendChild(li)
+}
+
+}
+
+     }
+
+
+   setAllNews() 
+
+
+const allCategories = async(category_id)=>{
+const url =`https://openapi.programming-hero.com/api/news/category/08`
+const res = await fetch(url)
+const data = await res.json()
+displayCategories(data.data)
+
+
+}
+
+
+allCategories()
+ 
+
+const displayCategories = categories=>{
+
+const categoriesContainer = document.getElementById("category-field")
+
+categories.forEach(category => {
+    const categoriesDiv = document.createElement("div")
+categoriesDiv.classList.add('col')
+categoriesDiv.innerHTML =`
+<div class="card mb-3 ms-5" style="max-width:full;">
+        
+         <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${category.image_url}" class="img-fluid rounded-start h-400px" alt="...">
+            
+            
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${category.title}</h5>
+              <p class="card-text">${category.details.slice(0,250)} </p>
+              <img src="${category.author.img}" class="rounded-circle img-fluid h-10px w-10px">
+              <span>${category.author.name}</span>
+              <p>${category.author.published_date}</p>
+            </div>
+          </div>
+        </div> 
+      </div>
+
+`
+categoriesContainer.appendChild(categoriesDiv)
+    
+});
+
+
+}
+
+
+
